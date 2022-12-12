22be60dd9bd52ee441c7b8247179cc551238de8c
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-12-12 22:46:07 +0530

    Setup express app

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import http from 'http'
+
+import app from './app'
+import * as config from './config'
+
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+  console.log(`Server is listening on port ${config.PORT}`)
+})
