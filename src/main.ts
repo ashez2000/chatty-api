b8edd5a9a111d50c404a177313893d63b7b9a555
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-12-13 12:18:37 +0530

    Connect to mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import http from 'http';
+import mongoose from 'mongoose';
 
 import app from './app';
 import * as config from './config';
 
-const server = http.createServer(app);
-server.listen(config.PORT, () => {
-  console.log(`Server is listening on port ${config.PORT}`);
+async function main() {
+  // Connect to MongoDB
+  mongoose.set('strictQuery', false);
+  const conn = await mongoose.connect(config.MONGO_URI);
+  console.log(
+    `Connected to MongoDB at ${conn.connection.host}:${conn.connection.port}/${conn.connection.name}`
+  );
+
+  // Start the server
+  const server = http.createServer(app);
+  server.listen(config.PORT, () => {
+    console.log(`Server is listening on port ${config.PORT}`);
+  });
+}
+
+main().catch((err) => {
+  console.error(err);
 });
