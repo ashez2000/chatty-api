8d4fe16c2113769ac54b525df369d91b22009e6b
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-12-18 05:52:07 +0530

    Refactor app with std and security middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,53 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import morgan from 'morgan';
+
 import 'express-async-errors';
 
-const app = express();
+export class ChattyApplication {
+  private readonly app: express.Application;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+  constructor(app: express.Application) {
+    this.app = app;
+  }
 
-export default app;
+  private setSecuirtyMiddleware() {
+    this.app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+      })
+    );
+
+    this.app.use(helmet());
+    this.app.use(hpp());
+
+    this.app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['key1', 'key2'],
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        secure: false,
+      })
+    );
+  }
+
+  private setStandardMiddleware() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(morgan('dev'));
+  }
+
+  private setErrorHandler() {}
+  private setRoutes() {}
+
+  start() {
+    this.setSecuirtyMiddleware();
+    this.setStandardMiddleware();
+    this.setErrorHandler();
+    this.setRoutes();
+  }
+}
